6e20a31a3e96aad77db0bd4e04ad13ef38f04a85
munabhusal <munabhusal323@gmail.com>
2024-05-16 21:12:28 +0545

    Best How sum

---
+++ SumwithNumbers/best_how_sum.py
@@ -0,0 +1,92 @@
+#to calculate the time
+import time  
+
+#Class to calculate the numbers which adds upto the targated number from the given list in the shortest possible way
+class FindBestHowOfSum:
+    def __init__(self):
+        pass
+
+# Without Using Dynamic Programming
+    def best_howsum_wodp(self, target, list ):
+        if(target == 0):
+            return []
+        if(target < 0):
+            return None      
+        best = None
+
+        for num in list:
+            remaining = target - num
+            ret_val = self.best_howsum_wodp(remaining , list)
+            if(ret_val is not None) :   
+                ret_val.append(num)
+                if(best is None or len(ret_val) < len(best)):
+                    best = ret_val.copy()
+        return(best)
+    
+#Using Dynamic programming Concept
+    def best_howsum_wdp(self, target, list, mydict = {}):
+        if(target in mydict):
+            return mydict[target]
+        if(target == 0):
+            return []
+        if(target < 0):
+            return None      
+        best = None
+        for num in list:
+            remaining = target - num
+            ret_val = self.best_howsum_wdp(remaining , list)
+            if(ret_val is not None) :  
+                new_node_point = ret_val + [num]
+
+                # What is diffrence between append and "+"?
+                # Consider,
+                '''
+                a = [1,3]
+                series = [1,2,3,9]
+                '''
+                # When using + operator 
+                '''
+                for num in series:
+                    combination = a + [num]
+                    print(combination)
+                '''
+                # Output:
+                '''
+                [1, 3, 1]
+                [1, 3, 2]
+                [1, 3, 3]
+                [1, 3, 9]
+                '''
+
+                #When Using append, value will get appended to the list one after the other
+                '''
+                for num in series:
+                    a.append(num)
+                    print(a)
+                '''
+                #Output
+                '''
+                [1, 3, 1]
+                [1, 3, 1, 2]
+                [1, 3, 1, 2, 3]
+                [1, 3, 1, 2, 3, 9]
+                '''
+    
+                if(best is None or len(new_node_point) < len(best)):
+                    # With respect to the length, output will if determined if sum exist from the given subset
+                    best = new_node_point.copy()        
+        mydict[target] = best
+        return(best)
+        
+obj = FindBestHowOfSum()
+
+start = time.time()
+res = obj.best_howsum_wdp(101, [5, 2 , 1])
+end = time.time()
+print(str(res) + '::  Using dynamic programming '+str(end-start))
+print ('____________________')
+
+start = time.time()
+res = obj.best_howsum_wodp(36, [5, 2 ])
+end = time.time()
+print(str(res) + '::  Without using dynamic programming '+str(end-start))
