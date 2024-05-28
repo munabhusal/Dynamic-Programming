422612ba7ac81196a60c579b83b5e1026145d56d
munabhusal <munabhusal323@gmail.com>
2024-05-29 04:42:36 +0545

    Solved using tabular method

---
+++ Fibonacci/fibonacci_table.py
@@ -0,0 +1,12 @@
+def fiboncci_table(n):
+    table = [0] * (n+1)
+    table[1] = 1
+
+    for i in range(n):
+        table[i+1] = table[i+1] + table[i]
+        if(i != n-1):
+            table[i+2] = table[i+2] + table[i]
+    
+    return(table[n])
+
+print(fiboncci_table(100))
