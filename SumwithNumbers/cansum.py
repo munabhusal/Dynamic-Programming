7231e2a3fb0d1094ea15919dd529bff44f7986ae
munabhusal <munabhusal323@gmail.com>
2024-06-08 23:20:42 +0545

    Tabular Approch added in can sum approch

--- SumwithNumbers/cansum.py
+++ SumwithNumbers/cansum.py
@@ -38,10 +38,20 @@ class CanSum:
         self.__mydict[remaining] = False
         return False
     
+    def check_if_sum_exist_dp_tabular(self, number, list):
+        table = [False] * (number+1)
+        table[0] =  True
+        for i in range(len(table)):
+            if table[i]== True:
+                for j in list:
+                    if(i+j <= number):
+                        table[i+j] = True        
+        return table[number]
+    
 myobj = CanSum()
 
 start = time.time()
-res = myobj.check_if_sum_exist_dp(200, [7, 14 ])
+res = myobj.check_if_sum_exist_dp(1500, [7, 14 ])
 end = time.time()
 print(str(res) + '::  Using dynamic programming '+str(end-start))
 
@@ -50,3 +60,9 @@ start = time.time()
 res = myobj.check_if_sum_exist_wodp(200, [7, 14 ])
 end = time.time()
 print(str(res) + '::  Without using dynamic programming '+str(end-start))
+
+
+start = time.time()
+res = myobj.check_if_sum_exist_dp_tabular(1500, [7, 14 ])
+end = time.time()
+print(str(res) + ':: Using dynamic programming tabular approch: '+str(end-start))
